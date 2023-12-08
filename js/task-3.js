73eec5ec5f39604ee9a27c5e9b969996e9bfd2e8
EmaxxD <97693129+EmaxxD@users.noreply.github.com>
2023-12-08 16:29:10 +0200

    another delete

--- js/task-3.js
+++ js/task-3.js
@@ -7,4 +7,3 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
 console.log(getElementWidth('200px', '0px', '0px')); // 200
-// console.log(getElementWidth('155.5px', '3.3px', '5.1px')); // 200
